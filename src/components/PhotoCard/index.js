5fbae47e7c9cbdb862468e83bf319e18eab1a001
Nycgia <pmunozb@protonmail.com>
2020-07-14 22:56:56 -0500

    Se crea lazy load básico

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,21 +1,40 @@
-import React from 'react'
-import { ImgWrapper, Img, Button } from './styles'
+import React, { useEffect, useRef, useState } from 'react'
+import { ImgWrapper, Img, Button, Article } from './styles'
 import { MdFavoriteBorder } from 'react-icons/md'
 
 const DEFAULT_IMG = 'https://res.cloudinary.com/midudev/image/upload/w_300/q_80/v1560262103/dogs.png'
 
 export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMG }) => {
-  return (
-    <article>
-      <a href={`/detail/${id}`}>
-        <ImgWrapper>
-          <Img src={src} />
-        </ImgWrapper>
-      </a>
+  const ref = useRef(null)
+  const [show, setShow] = useState(true)
 
-      <Button>
-        <MdFavoriteBorder size="32px" /> {likes} likes!
-      </Button>
-    </article>
+  useEffect(() => {
+    const observer = new window.IntersectionObserver((entries) => {
+      const { isIntersecting } = entries[0]
+      console.log(isIntersecting)
+      if (isIntersecting) {
+        setShow(true)
+        observer.disconnect()
+      }
+    })
+
+    observer.observe(ref.current)
+  }, [ref])
+
+  return (
+    <Article ref={ref}>
+      {show &&
+        <>
+          <a href={`/detail/${id}`}>
+            <ImgWrapper>
+              <Img src={src} />
+            </ImgWrapper>
+          </a>
+
+          <Button>
+            <MdFavoriteBorder size='32px' /> {likes} likes!
+          </Button>
+        </>}
+    </Article>
   )
 }
