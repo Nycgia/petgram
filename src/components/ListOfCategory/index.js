eecad6f8abda8377020c0868f3edef4fc9f25f2b
Nycgia <pmunozb@protonmail.com>
2020-07-14 22:12:38 -0500

    Se listan categorías desde api remota

--- src/components/ListOfCategory/index.js
+++ src/components/ListOfCategory/index.js
@@ -1,14 +1,42 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Category } from '../Category'
 import { List, Item } from './styles'
-import { categories } from '../../../api/db.json'
 
 export const ListOfCategory = () => {
-  return (
-    <List>
+  const [categories, setCategories] = useState([])
+  const [showFixed, setShowFixed] = useState(false)
+
+  useEffect(() => {
+    window.fetch('https://petgram-server-peter.nycgia.vercel.app/categories')
+      .then(res => res.json())
+      .then(response => {
+        setCategories(response)
+      })
+  }, [])
+
+  useEffect(() => {
+    const onScroll = e => {
+      const newShowFixed = window.scrollY >= 200
+      showFixed !== newShowFixed && setShowFixed(newShowFixed)
+    }
+
+    document.addEventListener('scroll', onScroll)
+
+    return () => document.removeEventListener('scroll', onScroll)
+  }, [showFixed])
+
+  const renderList = (fixed) => (
+    <List fixed={fixed}>
       {
         categories.map(category => <Item key={category.id}><Category {...category} /></Item>)
       }
     </List>
+  )
+
+  return (
+    <>
+      {renderList()}
+      {showFixed && renderList(true)}
+    </>
   )
 }
--- src/components/ListOfCategory/styles.js
+++ src/components/ListOfCategory/styles.js
@@ -1,10 +1,27 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
+import { fadeIn } from '../../styles/animation'
 
 export const List = styled.ul`
   list-style: none;
   display: flex;
-  overflow-x: scroll;
+  overflow-x: auto;
   width: 100%;
+
+  ${props => props.fixed && css`
+    ${fadeIn()}
+    background: #FFF;
+    border-radius: 60px;
+    box-shadow: 0 0 20px rgba(0, 0, 0, .3);
+    left: 0;
+    margin: 0 auto;
+    max-width: 600px;
+    padding: 5px;
+    position: fixed;
+    right: 0;
+    top: -20px;
+    transform: scale(.5);
+    z-index: 1;
+  `}
 `
 
 export const Item = styled.li`
