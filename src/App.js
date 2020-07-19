01a8362d0fd2c7cc2de853793b6d2777ce8cb155
Nycgia <pmunozb@protonmail.com>
2020-07-18 23:17:40 -0500

    Render props are added

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
 import React from 'react'
 import { ListOfCategory } from './components/ListOfCategory'
 import { ListOfPhotoCard } from './container/ListOfPhotoCard'
-
+import { PhotoCardWithQuery } from './container/PhotoCardWithQuery'
 import { Logo } from './components/Logo'
 import { GlobalStyle } from './styles/GlobalStyle'
 
-const App = () => (
-  <>
-    <Logo />
-    <GlobalStyle />
-    <ListOfCategory />
-    <ListOfPhotoCard categoryId={2} />
-  </>
-)
+const App = () => {
+  const urlParams = new window.URLSearchParams(window.location.search)
+  const detailId = urlParams.get('detail')
+
+  return (
+    <>
+      <Logo />
+      <GlobalStyle />
+      {
+        detailId
+          ? <PhotoCardWithQuery id={detailId} />
+          : <>
+            <ListOfCategory />
+            <ListOfPhotoCard categoryId={2} />
+          </>
+      }
+    </>)
+}
 
 export default App
