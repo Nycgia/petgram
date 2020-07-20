da217415157c544562e65466c9e034eb7fc4172c
Nycgia <pmunozb@protonmail.com>
2020-07-20 18:47:38 -0500

    Fix photo detail

--- src/App.js
+++ src/App.js
@@ -1,27 +1,20 @@
 import React from 'react'
-import { PhotoCardWithQuery } from './container/PhotoCardWithQuery'
 import { Logo } from './components/Logo'
 import { GlobalStyle } from './styles/GlobalStyle'
 import { Home } from './pages/Home'
+import { Detail } from './pages/Detail'
 import { Router } from '@reach/router'
 
-const App = () => {
-  const urlParams = new window.URLSearchParams(window.location.search)
-  const detailId = urlParams.get('detail')
-
-  return (
-    <>
-      <Logo />
-      <GlobalStyle />
-      {
-        detailId
-          ? <PhotoCardWithQuery id={detailId} />
-          : <Router>
-            <Home path='/' />
-            <Home path='/pet/:id' />
-          </Router>
-      }
-    </>)
-}
+const App = () => (
+  <>
+    <Logo />
+    <GlobalStyle />
+    <Router>
+      <Home path='/' />
+      <Home path='/pet/:id' />
+      <Detail path='/detail/:detailId' />
+    </Router>
+  </>
+)
 
 export default App
