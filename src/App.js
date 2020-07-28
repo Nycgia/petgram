0ff9a1dd7f0d390759af8db7784c8cf281b209ea
Nycgia <pmunozb@protonmail.com>
2020-07-28 13:12:12 -0500

    Se modifican rutas

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Logo } from './components/Logo'
 import { NavBar } from './components/NavBar'
 import { GlobalStyle } from './styles/GlobalStyle'
@@ -6,40 +6,34 @@ import { Home } from './pages/Home'
 import { Detail } from './pages/Detail'
 import { Favs } from './pages/Favs'
 import { User } from './pages/User'
+import { NotFound } from './pages/NotFound'
 import { NotRegisteredUser } from './pages/NotRegisteredUser'
 
-import { Router } from '@reach/router'
-import Context from './Context'
+import { Router, Redirect } from '@reach/router'
+import { Context } from './Context'
 
-const App = () => (
-  <>
-    <Logo />
-    <GlobalStyle />
-    <Router>
-      <Home path='/' />
-      <Home path='/pet/:id' />
-      <Detail path='/detail/:detailId' />
-    </Router>
-    <Context.Consumer>
-      {
-        ({ isAuth }) =>
-          isAuth
-            ? (
-              <Router>
-                <Favs path='/favs' />
-                <User path='/user' />
-              </Router>
-            )
-            : (
-              <Router>
-                <NotRegisteredUser path='/favs' />
-                <NotRegisteredUser path='/user' />
-              </Router>
-            )
-      }
-    </Context.Consumer>
-    <NavBar />
-  </>
-)
+const App = () => {
+  const { isAuth } = useContext(Context)
+
+  return (
+    <>
+      <Logo />
+      <GlobalStyle />
+      <Router>
+        <NotFound default/>
+        <Home path='/' />
+        <Home path='/pet/:id' />
+        <Detail path='/detail/:detailId' />
+        {!isAuth && <NotRegisteredUser path='/login' />}
+        {!isAuth && <Redirect from='/favs' to='/login' noThrow />}
+        {!isAuth && <Redirect from='/user' to='/login' noThrow />}
+        {isAuth && <Redirect from='/login' to='/' noThrow />}
+        <Favs path='/favs' />
+        <User path='/user' />
+      </Router>
+      <NavBar />
+    </>
+  )
+}
 
 export default App
